9b8bbfb0fc9c7107e959bfe64fc95ef24326f761
gunhee-jeong <kyle4619@naver.com>
2022-08-13 20:36:01 +0900

    코드숨 과제 1-2 간단한 계산기 만들기

---
+++ src/1주차/App(과제2).js
@@ -0,0 +1,91 @@
+/* eslint-disable react/react-in-jsx-scope, react/jsx-filename-extension, no-unused-vars */
+/* @jsx createElement */
+
+function createElement(tagName, props, ...children) {
+  const element = document.createElement(tagName);
+
+  Object.entries(props || {}).forEach(([key, value]) => {
+    element[key.toLowerCase()] = value;
+  });
+
+  children.flat().forEach((child) => {
+    if (child instanceof Node) {
+      element.appendChild(child);
+      return;
+    }
+    element.appendChild(document.createTextNode(child));
+  });
+
+  return element;
+}
+
+const calculateFunctions = {
+  '': (x, y) => x || y,
+  '=': (x, y) => x || y,
+  '+': (x, y) => x + y,
+  '-': (x, y) => x - y,
+  '*': (x, y) => x * y,
+  '/': (x, y) => x / y,
+};
+
+function calculate(operator, accumulator, number) {
+  return calculateFunctions[operator](accumulator, number);
+}
+
+const initialState = {
+  accumulator: 0,
+  number: 0,
+  operator: '',
+};
+
+function render({ accumulator, number, operator }) {
+  function handleClickReset() {
+    render(initialState);
+  }
+
+  function handleClickNumber(value) {
+    render({
+      accumulator,
+      number: number * 10 + value,
+      operator,
+    });
+  }
+
+  function handleClickOperator(value) {
+    render({
+      accumulator: calculate(operator, accumulator, number),
+      number: 0,
+      operator: value,
+    });
+  }
+
+  const element = (
+    <div>
+      <p>{number || accumulator}</p>
+      <p>
+        {[1, 2, 3, 4, 5, 6, 7, 8, 9, 0].map((i) => (
+          <button type="button" onClick={() => handleClickNumber(i)}>
+            {i}
+          </button>
+        ))}
+      </p>
+      <p>
+        {['+', '-', '*', '/', '='].map((i) => (
+          <button type="button" onClick={() => handleClickOperator(i)}>
+            {i}
+          </button>
+        ))}
+      </p>
+      <p>
+        <button type="button" onClick={() => handleClickReset()}>
+          reset
+        </button>
+      </p>
+    </div>
+  );
+
+  document.getElementById('app').textContent = '';
+  document.getElementById('app').appendChild(element);
+}
+
+render(initialState);
--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,4 @@
 import { createRoot } from 'react-dom/client';
 
-import App from './1주차/App';
-
 const add = createRoot(document.getElementById('app'));
 add.render(<App />);
