398b214f7b442afda11f6ea71378a616eec12a09
gunhee-jeong <kyle4619@naver.com>
2022-08-27 15:14:08 +0900

    테스트시 사용되  변수 restauransts의 중복을 제거하기 위해 restaurants를 모듈화

--- src/4주차과제2/Restaurants.test.jsx
+++ src/4주차과제2/Restaurants.test.jsx
@@ -4,13 +4,9 @@ import { render } from '@testing-library/react';
 
 import Restaurants from './Restaurants';
 
-test('Restaurants', () => {
-  const restaurants = [
-    {
-      id: 1, name: '김밥제국', category: '분식', address: '서울시 강남구',
-    },
-  ];
+import restaurants from '../../fixtures/restaurants';
 
+test('Restaurants', () => {
   const { getByText } = render((
     <Restaurants restaurants={restaurants} />
   ));
--- src/4주차과제2/RestaurantsContainer.test.jsx
+++ src/4주차과제2/RestaurantsContainer.test.jsx
@@ -6,13 +6,11 @@ import { render } from '@testing-library/react';
 
 import RestaurantsContainer from './RestaurantsContainer';
 
+import restaurants from '../../fixtures/restaurants';
+
 test('RestaurantsContainer', () => {
   useSelector.mockImplementation((selector) => selector({
-    restaurants: [
-      {
-        id: 1, name: '김밥제국', category: '분식', address: '서울시 강남구',
-      },
-    ],
+    restaurants,
   }));
 
   const { getByText } = render((
