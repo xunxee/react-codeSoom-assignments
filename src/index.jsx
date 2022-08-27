b874f5967755f80d8ba651f0239b5eac933771db
gunhee-jeong <kyle4619@naver.com>
2022-08-27 13:45:44 +0900

    4주차 과제2: App 컴포넌트를 간단히 화면에 보여주기 위한 테스

--- src/index.jsx
+++ src/index.jsx
@@ -2,11 +2,13 @@ import ReactDOM from 'react-dom/client';
 
 import { Provider } from 'react-redux';
 
+import App from './4주차과제2/App';
+
 // import App from './2주차/assignment1(creatingCounterApp)/App';
 // import App from './2주차/assignment2(creatingTodoApp)/App';
-import App from './4주차강의/App';
-
-import store from './4주차강의/store';
+// import App from './4주차강의/App';
+// import store from './4주차강의/store';
+import store from './4주차과제2/store';
 
 const app = ReactDOM.createRoot(document.getElementById('app'));
 app.render(
