0fae9ad89d4619ae6e66205ced82b34ed5b480ca
gunhee-jeong <kyle4619@naver.com>
2022-08-27 13:51:40 +0900

    4주차 과제2: App 컴포넌트를 간단히 화면에 추출하기 위한 리팩터링(Restaurants.jsx 추가)

--- src/4주차과제2/App.jsx
+++ src/4주차과제2/App.jsx
@@ -1,12 +1,10 @@
+import Restaurants from './restaurants';
+
 export default function App() {
   return (
     <div>
       <h1>Restaurants</h1>
-      <ul>
-        <li>
-          김밥제국
-        </li>
-      </ul>
+      <Restaurants />
     </div>
   );
 }
