d853718d06cdef5795e33e6c93ee87bc15d6a373
gunhee-jeong <kyle4619@naver.com>
2022-08-27 14:07:06 +0900

    4주차 과제2: Restaurants 컴포넌트에서 음식점 목록을 화면에 렌더링하기 위한 액션 구현

--- src/4주차과제2/App.jsx
+++ src/4주차과제2/App.jsx
@@ -1,10 +1,15 @@
 import Restaurants from './Restaurants';
 
 export default function App() {
+  const restaurants = [
+    {
+      id: 1, name: '김밥제국', category: '분식', address: '서울시 강남구',
+    },
+  ];
   return (
     <div>
       <h1>Restaurants</h1>
-      <Restaurants />
+      <Restaurants restaurants={restaurants} />
     </div>
   );
 }
--- src/4주차과제2/Restaurants.test.jsx
+++ src/4주차과제2/Restaurants.test.jsx
@@ -7,7 +7,7 @@ import Restaurants from './Restaurants';
 test('Restaurants', () => {
   const restaurants = [
     {
-      id: 1, title: '김밥제국', category: '분식', address: '서울시 강남구',
+      id: 1, name: '김밥제국', category: '분식', address: '서울시 강남구',
     },
   ];
 
