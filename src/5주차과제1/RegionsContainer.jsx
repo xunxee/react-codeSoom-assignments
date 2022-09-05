2cf499829889d0151eb51012a2766318a9117ceb
gunhee-jeong <kyle4619@naver.com>
2022-09-05 13:12:27 +0900

    RegionsContainer 컴포넌트에서 비동기로 받아온 data를 화면에 렌더링하는 액션을 테스트 및 구현

--- src/5주차과제1/RegionsContainer.jsx
+++ src/5주차과제1/RegionsContainer.jsx
@@ -1,7 +1,15 @@
+import { useSelector } from 'react-redux';
+
 export default function RegionsContainer() {
+  const regions = useSelector((state) => state.regions);
+
   return (
-    <div>
-      regions
-    </div>
+    <ul>
+      {regions.map((region) => (
+        <li key={region.id}>
+          {region.name}
+        </li>
+      ))}
+    </ul>
   );
 }
