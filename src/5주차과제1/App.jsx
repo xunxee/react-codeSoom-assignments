3215b555de3f8443d5c96c0427f7f9f2c84cba9c
gunhee-jeong <kyle4619@naver.com>
2022-09-05 13:38:33 +0900

    reducer 컴포넌트의 default 값을 추가하여 리팩터링 진행

--- src/5주차과제1/App.jsx
+++ src/5주차과제1/App.jsx
@@ -2,6 +2,8 @@ import { useEffect } from 'react';
 
 import { useDispatch } from 'react-redux';
 
+import RegionsContainer from './RegionsContainer';
+
 import {
   loadInitialData,
 } from './actions';
@@ -10,10 +12,6 @@ import {
 // 1. 지역 선택 - Regions <- API
 // 2. 분류 선택 - Category <- API
 // 3. 식당 목록 - Restaurants <- API (with region, category)
-
-function RegionsContainer() {
-  return null;
-}
 
 function CategoriesContainer() {
   return null;
@@ -27,7 +25,7 @@ export default function App() {
   const dispatch = useDispatch();
 
   useEffect(() => {
-    dispatch(loadInitialData());
+    dispatch(loadInitialData(dispatch));
   }, []);
 
   return (
