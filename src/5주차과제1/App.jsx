d8c527ed58823ff8c9ff3a8c9ddda1e0499ce68f
gunhee-jeong <kyle4619@naver.com>
2022-09-05 20:18:30 +0900

    CategoriesContainer 컴포넌트를 App 컴포넌트에 연결하는 리팩터링

--- src/5주차과제1/App.jsx
+++ src/5주차과제1/App.jsx
@@ -3,6 +3,7 @@ import { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 
 import RegionsContainer from './RegionsContainer';
+import CategoriesContainer from './CategoriesContainer';
 
 import {
   loadInitialData,
@@ -12,10 +13,6 @@ import {
 // 1. 지역 선택 - Regions <- API
 // 2. 분류 선택 - Category <- API
 // 3. 식당 목록 - Restaurants <- API (with region, category)
-
-function CategoriesContainer() {
-  return null;
-}
 
 function RestaurantsContainer() {
   return null;
