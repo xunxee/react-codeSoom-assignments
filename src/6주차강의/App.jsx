7996bd9fa40e3943e191cd1d2316b6b342acab5d
gunhee-jeong <kyle4619@naver.com>
2022-09-13 14:07:53 +0900

    App 컴포넌트에서 pathname이 default일 때 Home이 되도록 구현

--- src/6주차강의/App.jsx
+++ src/6주차강의/App.jsx
@@ -1,6 +1,14 @@
 import RestaurantsPage from './RestaurantsPage';
 
 export default function App() {
+  const { location: { pathname } } = window;
+
+  if (pathname === '/') {
+    return (
+      <p>Home</p>
+    );
+  }
+
   return (
     <RestaurantsPage />
   );
