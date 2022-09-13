93dd6145ab6ccb2f0c46874fbf889cc5899b3149
gunhee-jeong <kyle4619@naver.com>
2022-09-13 12:56:15 +0900

    App 컴포넌트에 RestaurantsPage 컴포넌트를 연결

--- src/6주차강의/App.jsx
+++ src/6주차강의/App.jsx
@@ -1,32 +1,7 @@
-import { useEffect } from 'react';
-
-import { useDispatch } from 'react-redux';
-
-import RegionsContainer from './RegionsContainer';
-import CategoriesContainer from './CategoriesContainer';
-import RestaurantsContainer from './RestaurantsContainer';
-
-import {
-  loadInitialData,
-} from './actions';
-
-// 0. 지역, 분류 목록을 얻기
-// 1. 지역 선택 - Regions <- API
-// 2. 분류 선택 - Category <- API
-// 3. 식당 목록 - Restaurants <- API (with region, category)
+import RestaurantsPage from './RestaurantsPage';
 
 export default function App() {
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    dispatch(loadInitialData(dispatch));
-  }, []);
-
   return (
-    <div>
-      <RegionsContainer />
-      <CategoriesContainer />
-      <RestaurantsContainer />
-    </div>
+    <RestaurantsPage />
   );
 }
