be36a001409790348224befb541b727ef0c06033
gunhee-jeong <kyle4619@naver.com>
2022-09-14 18:07:15 +0900

    react-router-dom을 사용하여 App 컴포넌트의 라우팅 초기 설정

--- src/6주차강의/App.jsx
+++ src/6주차강의/App.jsx
@@ -1,18 +1,23 @@
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from 'react-router-dom';
+
 import HomePage from './HomePage';
 import AboutPage from './AboutPage';
 import NotFoundPage from './NotFoundPage';
 import RestaurantsPage from './RestaurantsPage';
 
 export default function App() {
-  const { location: { pathname } } = window;
-
-  const MyComponent = {
-    '/': HomePage,
-    '/about': AboutPage,
-    '/restaurants': RestaurantsPage,
-  }[pathname] || NotFoundPage;
-
   return (
-    <MyComponent />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/about" element={<AboutPage />} />
+        <Route path="/restaurants" element={<RestaurantsPage />} />
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
