020174b380c3e9325b323d9848115e6cde6a4151
gunhee-jeong <kyle4619@naver.com>
2022-09-17 23:36:36 +0900

    Reducer의 setRestaurant의 액션에 대한 테스트 및 구현

--- src/6주차과제/RestaurantPage.test.jsx
+++ src/6주차과제/RestaurantPage.test.jsx
@@ -1,8 +1,22 @@
 import { render } from '@testing-library/react';
 
+import { useDispatch, useSelector } from 'react-redux';
+
 import RestaurantPage from './RestaurantPage';
 
 describe('RestaurantPage', () => {
+  beforeEach(() => {
+    const dispatch = jest.fn();
+
+    useDispatch.mockImplementation(() => dispatch);
+
+    useSelector.mockImplementation((state) => state({
+      restaurant: {
+        id: 1,
+        name: '마법사주방',
+      },
+    }));
+  });
   it('renders name', () => {
     const params = { id: '1' };
 
