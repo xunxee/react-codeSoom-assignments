8893a5fe112088f77cbe7bf7d352678f0b6e859a
gunhee-jeong <kyle4619@naver.com>
2022-09-18 17:47:48 +0900

    MenuItems 컴포넌트를 위한 액션을 테스트 및 구현

--- src/6주차과제/RestaurantDetail.jsx
+++ src/6주차과제/RestaurantDetail.jsx
@@ -1,18 +1,4 @@
-function MenuItems({ menuItems }) {
-  if (!menuItems || menuItems.length === 0) {
-    return (<p>메뉴가 없어요!</p>);
-  }
-
-  return (
-    <ul>
-      {menuItems.map((menuItem) => (
-        <li key={menuItem.id}>
-          {menuItem.name}
-        </li>
-      ))}
-    </ul>
-  );
-}
+import MenuItems from './MenuItems';
 
 export default function RestaurantDetail({ restaurant }) {
   const { name, address, menuItems } = restaurant;
---
+++ src/MenuItems.test.jsx
@@ -0,0 +1,31 @@
+import { render } from '@testing-library/react';
+
+import MenuItems from './6주차과제/MenuItems';
+
+describe('MenuItems', () => {
+  context('with menu items', () => {
+    it('renders menu items', () => {
+      const menuItems = [
+        { id: 1, name: '공기밥' },
+      ];
+
+      const { container } = render(
+        <MenuItems menuItems={menuItems} />,
+      );
+
+      expect(container).toHaveTextContent('공기밥');
+    });
+  });
+
+  context('without menu items', () => {
+    it('renders no items message', () => {
+      const menuItems = [];
+
+      const { container } = render(
+        <MenuItems menuItems={menuItems} />,
+      );
+
+      expect(container).toHaveTextContent('없어요!');
+    });
+  });
+});
