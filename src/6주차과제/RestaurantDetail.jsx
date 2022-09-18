22baf73783171bebfad1db848211fb750bffa5a6
gunhee-jeong <kyle4619@naver.com>
2022-09-18 16:19:27 +0900

    RestaurantDetail에 메뉴 목록이 보이도록 리팩터링

--- src/6주차과제/RestaurantDetail.jsx
+++ src/6주차과제/RestaurantDetail.jsx
@@ -1,8 +1,22 @@
 export default function RestaurantDetail({ restaurant }) {
+  const { name, address, menuItems } = restaurant;
+
   return (
     <div>
-      <p>{restaurant.name}</p>
-      <p>{restaurant.address}</p>
+      <h2>{name}</h2>
+      <p>
+        주소:
+        {' '}
+        {address}
+      </p>
+      <h3>메뉴</h3>
+      <ul>
+        {menuItems.map((menuItem) => (
+          <li key={menuItem.id}>
+            {menuItem.name}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
