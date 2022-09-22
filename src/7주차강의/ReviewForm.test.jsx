98d98e34708a1425fd7f481e87a66895fb063abb
gunhee-jeong <kyle4619@naver.com>
2022-09-22 18:24:56 +0900

    ReveiwForm 의 onChange 이벤트에 대한 테스트를 추가하는 리팩터링

--- src/7주차강의/ReviewForm.test.jsx
+++ src/7주차강의/ReviewForm.test.jsx
@@ -1,4 +1,4 @@
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 
 import ReviewForm from './ReviewForm';
 
@@ -12,5 +12,30 @@ describe('ReviewForm', () => {
 
     expect(queryByLabelText('평점')).not.toBeNull();
     expect(queryByLabelText('리뷰 내용')).not.toBeNull();
+  });
+
+  it('listens change events', () => {
+    const handleChange = jest.fn();
+
+    const { getByLabelText } = render(
+      <ReviewForm onChange={handleChange} />,
+    );
+
+    const controls = [
+      { label: '평점', name: 'score', value: '5' },
+      {
+        label: '리뷰 내용',
+        name: 'description',
+        value: '정말 최고 :)',
+      },
+    ];
+
+    controls.forEach(({ label, name, value }) => {
+      fireEvent.change(getByLabelText(label), {
+        target: { value },
+      });
+
+      expect(handleChange).toBeCalledWith({ name, value });
+    });
   });
 });
